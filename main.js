dcfe774e3e14df0517d10607234f7f740e29dcaf
Andres Felipe Chaparro Carvajal <andreschc9@gmail.com>
2022-05-18 23:50:39 -0500

    Stop timer is not working

--- main.js
+++ main.js
@@ -18,38 +18,94 @@ startButton.setAttribute('id', 'startButton');
 startButton.innerText = "Start timer";
 startButton.style.padding = "10px";
 startButton.style.border = 'none';
-startButton.style.left = '40%';
+startButton.style.left = '25%';
 startButton.style.position = 'relative';
 startButton.style.top = '9.5px';
 startButton.style['border-radius'] = "8px";
 startButton.style.padding = "10px";
 startButton.style['background-color'] = "#e8c349";
+startButton.style.color = 'black';
+startButton.addEventListener('click', timer);
 
 pomodoro.appendChild(startButton);
 
 const minutes = document.createElement('span');
-const 
+const semicolon = document.createElement('span');
 const seconds = document.createElement('span');
 
 counter.setAttribute('id', 'counter');
-counter.innerText = '25:00';
-counter.style.fontSize = '15px';
+minutes.innerText = '25';
+semicolon.innerText = ':'
+seconds.innerText = '00';
+counter.style.fontSize = '50px';
+counter.style.position = 'absolute';
+counter.style.height = '60px'
+counter.style.left = '45%';
+counter.style.top = '-10px';
+counter.style.color = 'white';
+
+counter.appendChild(minutes);
+counter.appendChild(semicolon);
+counter.appendChild(seconds);
 
 pomodoro.appendChild(counter);
 
-let counter = 0;
+endButton.setAttribute('id', 'endButton');
+endButton.innerText = "Stop timer";
+endButton.style.padding = "10px";
+endButton.style.border = 'none';
+endButton.style.left = '60%';
+endButton.style.position = 'relative';
+endButton.style.top = '9.5px';
+endButton.style['border-radius'] = "8px";
+endButton.style.padding = "10px";
+endButton.style['background-color'] = "rgb(181,81,74)";
+endButton.style.color = 'white';
+endButton.addEventListener('click', stop);
+
+pomodoro.appendChild(endButton);
+
+const gif = document.createElement('img');
+gif.setAttribute('id', 'homergif');
+gif.setAttribute('src', 'https://c.tenor.com/1yn3cUEhDx4AAAAC/homer-simpson-the-simpsons.gif');
+gif.style.position = 'absolute';
+gif.style.width = '70%';
+gif.style.height = '80%';
+gif.style.left = '15%';
+gif.style.top = '100px';
+gif.style['z-index'] = '99';
 
 function timer(){
-    const countDownTime25 = 1500000;
-    const countDownTime5 = 300000;
+    let countDownTime25 = 3000;
+    let countDownTime5 = 3000;
+    function modifier(){
+        if(countDownTime5 < 0) {
+            countDownTime25 = 1500000;
+            countDownTime5 = 300000;
+            body.removeChild(document.querySelector('#homergif'));
+        }
+        if(countDownTime25 > 0){
+            minutes.innerText = Math.floor((countDownTime25 % (1000 * 60 * 60)) / (1000 * 60)) < 10 ? `0${Math.floor((countDownTime25 % (1000 * 60 * 60)) / (1000 * 60))}` : Math.floor((countDownTime25 % (1000 * 60 * 60)) / (1000 * 60));
+            seconds.innerText = Math.floor((countDownTime25 % (1000 * 60)) / 1000) < 10 ? `0${Math.floor((countDownTime25 % (1000 * 60)) / 1000)}` : Math.floor((countDownTime25 % (1000 * 60)) / 1000);
+            countDownTime25 -= 1000;
+            console.log(countDownTime25);
+        } else {
+            minutes.innerText = Math.floor((countDownTime5 % (1000 * 60 * 60)) / (1000 * 60)) < 10 ? `0${Math.floor((countDownTime5 % (1000 * 60 * 60)) / (1000 * 60))}` : Math.floor((countDownTime5 % (1000 * 60 * 60)) / (1000 * 60));
+            seconds.innerText = Math.floor((countDownTime5 % (1000 * 60)) / 1000) < 10 ? `0${Math.floor((countDownTime5 % (1000 * 60)) / 1000)}` : Math.floor((countDownTime5 % (1000 * 60)) / 1000);
+            console.log(countDownTime5);
+            countDownTime5 -= 1000;
+            body.prepend(gif);
+        }
+        let timeourID = setTimeout(modifier, 1000);
+        return timeourID;
+    }
+    modifier();
+    let timeoutId = setTimeout(timer, 1800000);
+    return timeoutId;
+}
 
-    
-
-    
-
-
-
-    setTimeout(timer, 1800000);
+function stop(timeourID){
+    clearTimeout(timeourID);
 }
 
 
